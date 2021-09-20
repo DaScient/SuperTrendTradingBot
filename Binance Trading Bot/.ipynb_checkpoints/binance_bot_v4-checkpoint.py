f80c96c9fbf61cb36d28b754f8a9416158984832
donut.ipynb <25983786+Donifide@users.noreply.github.com>
2021-09-20 14:39:25 -0700

    Fixed min_sell_price issue.

--- Binance Trading Bot/.ipynb_checkpoints/binance_bot_v4-checkpoint.py
+++ Binance Trading Bot/.ipynb_checkpoints/binance_bot_v4-checkpoint.py
@@ -21,37 +21,6 @@ exchange = ccxt.binanceus({
 "secret": config.BINANCE_SECRET,
 'enableRateLimit': True})
 
-#Supertrend formula
-def tr(data):
-    data['previous_close'] = data['close'].shift(1)
-    data['high-low'] = abs(data['high'] - data['low'])
-    data['high-pc'] = abs(data['high'] - data['previous_close'])
-    data['low-pc'] = abs(data['low'] - data['previous_close'])
-    tr = data[['high-low', 'high-pc', 'low-pc']].max(axis=1)
-    return tr
-def atr(data, period):
-    data['tr'] = tr(data)
-    atr = data['tr'].rolling(period).mean()
-    return atr
-def supertrend(df, period=7, atr_multiplier=3):
-    hl2 = (df['high'] + df['low']) / 2
-    df['atr'] = atr(df, period)
-    df['upperband'] = hl2 + (atr_multiplier * df['atr'])
-    df['lowerband'] = hl2 - (atr_multiplier * df['atr'])
-    df['in_uptrend'] = True
-    for current in range(1, len(df.index)):
-        previous = current - 1
-        if df['close'][current] > df['upperband'][previous]:
-            df['in_uptrend'][current] = True
-        elif df['close'][current] < df['lowerband'][previous]:
-            df['in_uptrend'][current] = False
-        else:
-            df['in_uptrend'][current] = df['in_uptrend'][previous]
-            if df['in_uptrend'][current] and df['lowerband'][current] < df['lowerband'][previous]:
-                df['lowerband'][current] = df['lowerband'][previous]
-            if not df['in_uptrend'][current] and df['upperband'][current] > df['upperband'][previous]:
-                df['upperband'][current] = df['upperband'][previous]
-    return df
 
 #Instance parameters
 name=input("Your name: ")
@@ -77,7 +46,7 @@ if timeframe == "15m":
     a = 700
     b = 900
 
-#Supertrend
+#Supertrend formula
 def tr(data):
     data['previous_close']=data['close'].shift(1)
     data['high-low']=abs(data['high']-data['low'])
@@ -108,6 +77,7 @@ def supertrend(df,period=7,atr_multiplier=volatility):
             if not df['in_uptrend'][current] and df['upperband'][current]>df['upperband'][previous]:
                 df['upperband'][current]=df['upperband'][previous]
     return df
+
 #Analysis & decision making
 def check_buy_sell_signals(df):
     global in_position,order_size,ticker,timeframe,trade_amount,min_sell_price,markup,prev_purch_price,max_loss
@@ -137,7 +107,7 @@ def check_buy_sell_signals(df):
         bar = exchange.fetch_ohlcv(f'{ticker}', timeframe="1m", limit=1)
         price = float(bar[-1][3]) #identifies current low price
         print("Changed to downtrend. Attempting sale.")
-        if in_position and min_sell_price*(1-max_loss)>price>min_sell_price*markup:
+        if in_position and min_sell_price*(1-max_loss)<price:
             order = exchange.create_market_sell_order(f'{ticker}',order_size)
             print('Status:'+order['info']['status'],
                   'Price:'+order['trades'][0]['info']['price'],
